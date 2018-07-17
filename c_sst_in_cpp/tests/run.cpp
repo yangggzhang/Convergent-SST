d7fbadf45bdc18b1642189f27c1005da1cc48f2b
zyyyyyyyyy <youngzh@umich.edu>
2018-07-17 17:07:49 -0400

    delete useless system files

--- c_sst_in_cpp/tests/run.cpp
+++ c_sst_in_cpp/tests/run.cpp
@@ -15,12 +15,6 @@
 #include "utilities/random.hpp"
 #include "utilities/timer.hpp"
 
-#include "systems/pendulum.hpp"
-#include "systems/point.hpp"
-#include "systems/car.hpp"
-#include "systems/rally_car.hpp"
-#include "systems/cart_pole.hpp"
-#include "systems/two_link_acrobot.hpp"
 #include "systems/climb_hill.hpp"
 #include "motion_planners/sst.hpp"
 #include "motion_planners/rrt.hpp"
